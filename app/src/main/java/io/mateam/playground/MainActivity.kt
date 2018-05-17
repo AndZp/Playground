da2262c1119a8e12ae01861ceb45c4adc58f38ea
DESKTOP-HSV4S2K\andre <andreyo@gmedinnovations.com>
2018-05-17 14:06:43 +0300

    Initial commit

---
+++ app/src/main/java/io/mateam/playground/MainActivity.kt
@@ -0,0 +1,19 @@
+package io.mateam.playground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.mateam.playground.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
